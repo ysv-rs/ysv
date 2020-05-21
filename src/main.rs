d2c441eb92ab7952100a3ab5a02c42e31913a282
Anatoly Scherbakov <altaisoft@gmail.com>
2020-05-21 12:10:37 +0700

    Print headers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::io;
+// use std::env;
+// use std::error::Error;
+// use std::ffi::OsString;
+// use std::fs::File;
+use csv::Reader;
+
+
+fn pass_through() {
+    let mut reader = Reader::from_reader(io::stdin());
+
+    let headers = reader.headers().expect("Where are my headers?!").clone();
+    println!("Headers: {:?}", headers);
+
+    for result in reader.records() {
+        match result {
+            Ok(record) => println!("{:?}", record),
+            Err(err) => {
+                eprintln!("ERROR reading CSV from <stdin>: {}", err);
+            }
+        }
+    }
+}
 
 
 fn main() {
-    let mut reader = csv::Reader::from_reader(io::stdin());
+    // let mut reader = match env::args_os().nth(1) {
+    //    None => ,
+    //    Some(file_path) => Reader::from_path(file_path).expect("Cannot open a file!"),
+    // }
 
-    for result in reader.records() {
-        let record = result.expect("a CSV record");
-        println!("{:?}", record);
-    }
+    pass_through();
 }
