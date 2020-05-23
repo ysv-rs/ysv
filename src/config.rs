09cc231b0c2b47393f8a6a9131120847478aa753
Anatoly Scherbakov <altaisoft@gmail.com>
2020-05-23 20:21:16 +0700

    Refactored into config.rs

---
+++ src/config.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::env;
+use std::fs;
+use csv::{Reader, StringRecord};
+use csv::ByteRecord;
+use std::ffi::OsString;
+use serde::Deserialize;
+use std::collections::BTreeMap;
+
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Transformation {
+    Input { input: String },
+    Trim { trim: u16 },
+}
+
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Column {
+    Input(String),
+    Transformations(Vec<Transformation>),
+}
+
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    version: i8,
+    columns: BTreeMap<String, Column>,
+}
+
+
+pub fn parse_config_from_file(path: OsString) -> Result<Config, String> {
+    match fs::read_to_string(path) {
+        Ok(content) => match serde_yaml::from_str(&content) {
+            Ok(config) => Ok(config),
+            Err(err) => Err(format!("Could not parse YAML: {}", err.to_string()))
+        },
+        Err(err) => Err(format!("Cannot open config: {}", err.to_string()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod config;
+
 use std::io;
 use std::env;
 use std::fs;
@@ -7,28 +9,7 @@ use std::ffi::OsString;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 
-
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum Transformation {
-    Input { input: String },
-    Trim { trim: u16 },
-}
-
-
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum Column {
-    Input(String),
-    Transformations(Vec<Transformation>),
-}
-
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    version: i8,
-    columns: BTreeMap<String, Column>,
-}
+use config::{Config, parse_config_from_file};
 
 
 fn transform(record: ByteRecord, config: &Config, headers: &StringRecord) -> ByteRecord {
@@ -57,17 +38,6 @@ fn process(config: Config) {
     }
 
     writer.flush().expect("cannot flush!");
-}
-
-
-fn parse_config_from_file(path: OsString) -> Result<Config, String> {
-    match fs::read_to_string(path) {
-        Ok(content) => match serde_yaml::from_str(&content) {
-            Ok(config) => Ok(config),
-            Err(err) => Err(format!("Could not parse YAML: {}", err.to_string()))
-        },
-        Err(err) => Err(format!("Cannot open config: {}", err.to_string()))
-    }
 }
 
 
