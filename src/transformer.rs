1f515816624195cbdf33a1b5b89e8aea6efb2dd8
Anatoly Scherbakov <altaisoft@gmail.com>
2020-05-25 02:39:55 +0700

    works and outputs?!

--- src/transformer.rs
+++ src/transformer.rs
@@ -14,10 +14,18 @@ pub struct Transformer {
 }
 
 
+fn safe_to_utf8(bytes: &[u8]) -> String {
+    match String::from_utf8(bytes.to_vec()) {
+        Ok(value) => value,
+        Err(err) => String::new(),
+    }
+}
+
+
 impl Expression {
-    pub fn apply(self, value: Option<String>, row: &ByteRecord) -> Option<String> {
+    pub fn apply(&self, value: Option<String>, row: &ByteRecord) -> Option<String> {
         match self {
-            Expression::Input(index) => Some(String::from(row[index])),
+            Expression::Input(index) => Some(safe_to_utf8(&row[*index])),
             Expression::Slice { start, end } => match value {
                 Some(content) => Some(content),
                 None => None
